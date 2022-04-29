e99a24e365b818507bf9e2491282101604b1766b
true4 <true4pjh@naver.com>
2022-04-30 00:40:27 +0900

    add exception handler

---
+++ src/test/java/com/tistory/hitomis/springboot_practice1/controller/Practice1ControllerTest.java
@@ -0,0 +1,62 @@
+package com.tistory.hitomis.springboot_practice1.controller;
+
+import com.tistory.hitomis.springboot_practice1.dto.DeveloperDto;
+import com.tistory.hitomis.springboot_practice1.service.DeveloperService;
+import com.tistory.hitomis.springboot_practice1.type.DeveloperLevel;
+import com.tistory.hitomis.springboot_practice1.type.DeveloperSkillType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(Practice1Controller.class)
+class Practice1ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DeveloperService developerService;
+
+    protected MediaType contentType =
+            new MediaType(MediaType.APPLICATION_JSON.getType(),
+                    MediaType.APPLICATION_JSON.getSubtype(),
+                    StandardCharsets.UTF_8);
+
+    @Test
+    void getAllDevelopers() throws Exception {
+        DeveloperDto developerDto = DeveloperDto.builder()
+                .developerSkillType(DeveloperSkillType.BACK_END)
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .memberId("memberId1").build();
+
+        DeveloperDto developerDto2 = DeveloperDto.builder()
+                .developerSkillType(DeveloperSkillType.FRONT_END)
+                .developerLevel(DeveloperLevel.SENIOR)
+                .memberId("memberId2").build();
+
+        given(developerService.getAllEmployedDevelopers())
+                .willReturn(Arrays.asList(developerDto, developerDto2));
+
+        mockMvc.perform(get("/developers").contentType(contentType))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(
+                        jsonPath("$.[0].developerSkillType",
+                                is(DeveloperSkillType.BACK_END.name()))
+                );
+    }
+}
